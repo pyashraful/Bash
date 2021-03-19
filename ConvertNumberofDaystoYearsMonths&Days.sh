87b687da46c65eeaec0d484b36ce68e6ada7be3f
Md Ashraful Islam <ashrafulislam180@gmail.com>
2021-03-19 17:26:51 +0000

    Convert Number of Days to Years, Months & Days in bash

--- ConvertNumberofDaystoYearsMonths&Days.sh
+++ ConvertNumberofDaystoYearsMonths&Days.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+#Convert Number of Days to Years, Months & Days
 echo "Enter number"
 read int
 
-y=$(($int/360))
-m=$(($(($int-$(($y*360))))/30))
-d=$(($int-$(($(($y*360))+$(($m*30))))))
+y=$(($int/360)) #Calculating years
+m=$(($(($int-$(($y*360))))/30)) #Calculating Months
+d=$(($int-$(($(($y*360))+$(($m*30)))))) #Calculating yeaDaysrs
 echo $y
 echo $m
 echo $d
